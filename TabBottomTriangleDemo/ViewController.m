3a00e72d4bb4aaf55f8aa5abf611744a964e3220
zhaorui <zhaoruiexplorer@icloud.com>
2017-03-14 10:12:54 +0800

    Initial Commit

---
+++ TabBottomTriangleDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TabBottomTriangleDemo
+//
+//  Created by 赵睿 on 14/03/2017.
+//  Copyright © 2017 com.homebrew.zhaorui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+  [super setRepresentedObject:representedObject];
+
+  // Update the view, if already loaded.
+}
+
+
+@end
---
+++ TabBottomTriangleDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TabBottomTriangleDemo
+//
+//  Created by 赵睿 on 14/03/2017.
+//  Copyright © 2017 com.homebrew.zhaorui. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
